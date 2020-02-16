de4ae575ae2ac08fd9f780483ac64c5725e49620
Pavel Kas <pavkascool@gmail.com>
2020-02-16 18:17:25 +0300

    before final homework

---
+++ c17_anim/src/main/java/com/gmail/pavkascool/c17_anim/MainActivity.java
@@ -0,0 +1,14 @@
+package com.gmail.pavkascool.c17_anim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
